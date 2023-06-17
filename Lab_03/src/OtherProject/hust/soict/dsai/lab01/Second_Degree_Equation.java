7bcea51f59b449034f9384ce0f7050378c42fd0a
Bùi Phương Nam <phuongnam2442003@gmail.com>
2023-06-18 01:58:15 +0700

    lab 03

---
+++ Lab_03/src/OtherProject/hust/soict/dsai/lab01/Second_Degree_Equation.java
@@ -0,0 +1,31 @@
+package OtherProject.hust.soict.dsai.lab01;
+
+import java.lang.Math;
+import java.util.Scanner;
+
+public class Second_Degree_Equation {
+    public static void main(String[] args) {
+        double a, b, c, delta;
+        Scanner Obj = new Scanner(System.in);
+        System.out.println("Enter a: ");
+        a = Obj.nextDouble();
+        System.out.println("Enter b: ");
+        b = Obj.nextDouble();
+        System.out.println("Enter c: ");
+        c = Obj.nextDouble();
+        if(a == 0){
+            if(b == 0 && c != 0) System.out.println("Vo nghiem");
+            if(b == 0 && c == 0) System.out.println("Vo so nghiem");
+            if(b != 0) System.out.println(-b/a);
+        }else{
+            delta = b*b - 4*a*c;
+            if(delta == 0){
+                System.out.println("Nghiem boi 2: "+-b/2*a);
+            }else{
+                System.out.println("Nghiem 1: "+(-b+Math.sqrt(delta))/(2*a));
+                System.out.println("Nghiem 2: "+(-b-Math.sqrt(delta))/(2*a));
+            }
+
+        }
+    }
+}
